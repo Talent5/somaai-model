bd4cf49f8365dc18302a7a466ac079f10866b8df
Talent Mundwa <146452683+Talent5@users.noreply.github.com>
2024-09-24 14:38:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ except Exception as e:
 # --- Scheduler ---
 scheduler = BackgroundScheduler()
 
-@scheduler.scheduled_job(IntervalTrigger(hours=24))
+@scheduler.scheduled_job(IntervalTrigger(hours=0.02))
 def run_recommendation_job():
     """Generate and save recommendations for all users."""
     with app.app_context():
